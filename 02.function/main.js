06c4d81e52fab74a5b3d85c3c063db5b3e155579
woojoung <ywj981217@naver.com>
2024-01-15 17:32:59 +0900

    study day02

---
+++ 02.function/main.js
@@ -0,0 +1,66 @@
+
+
+
+// function hello(message, absolute) {
+//   return message + " is not everyting to solve problem" + absolute;
+// }
+
+// let result = hello("test", " but it will be good to solve problems")
+// console.log(result)
+
+
+// const hello2 = (hello) => {
+//   return hello + 123;
+// }
+// console.log(hello2("lovely"))
+
+
+// let greet = function () {
+//   console.log("Greetings!");
+// };
+
+// function sum(a, b) {
+//   return (a + b);    //retunr 매개변수 a + b 값 을 반환 
+// }
+
+// let e = sum(1, 2);   //변수 선언 후 함수 호출 인수 값이 더해져 변수 e로 넘어감 
+// let f = sum(10, 2);
+// let g = sum(5, 5);
+
+// console.log(e);
+// console.log(f);   //console화면에 출력 
+// console.log(g);
+
+
+// 사용자로부터 성적 입력
+// let scoreInput = prompt("성적을 입력하세요:");
+
+// // 입력된 성적을 실수로 변환
+// let score = parseFloat(scoreInput);
+
+// // 스위치문을 사용하여 등급 결정
+// let grade;
+
+// switch (true) {
+//   case isNaN(score):
+//     grade = "유효한 숫자를 입력하세요.";
+//     break;
+//   case score >= 90:
+//     grade = "A";
+//     break;
+//   case score >= 80:
+//     grade = "B";
+//     break;
+//   case score >= 70:
+//     grade = "C";
+//     break;
+//   case score >= 60:
+//     grade = "D";
+//     break;
+//   default:
+//     grade = "F";
+//     break;
+// }
+
+// // 결과 출력
+// alert("입력한 성적의 등급은 " + grade + "입니다.");
