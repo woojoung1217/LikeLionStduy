4e1c838153e7a4d1bfd79100b7286085bb89aed3
woojoung <ywj981217@naver.com>
2024-01-16 23:10:40 +0900

    study03

--- 03.VOD/main.js
+++
@@ -1,9 +0,0 @@
-
-document.write("코드라이언 짱짱")
-
-
-
-var num = Math.random() * 45 + 1;
-var ball1 = parseInt(num);
-
-
---
+++ 04.study3/main.js
@@ -0,0 +1,46 @@
+
+
+// function reverseArray(arr) {
+
+//   const reversedArr = arr.reverse();
+//   return reversedArr;
+// }
+
+// const originalArray = [1, 2, 3, 4, 5];
+// const reversedArray = reverseArray(originalArray);
+// console.log("뒤집힌 배열:", reversedArray);
+
+
+function reverseArray(arr) {
+  // 배열의 복사본을 만들어 역순으로 정렬
+  const reversedArr = arr.slice().sort((a, b) => b - a);
+  return reversedArr;
+}
+
+
+
+// 테스트
+const originalArray = [1, 2, 3, 4, 5];
+const reversedArray = reverseArray(originalArray);
+console.log("뒤집힌 배열:", reversedArray);
+
+
+
+
+
+
+
+
+
+// function reversDouble(arr) {
+//   // 배열의 복사본을 만들어 역순으로 정렬
+//   const reversedArr = arr.slice().reverse();
+
+//   // 각 원소에 2를 곱함
+//   for (let i = 0; i < reversedArr.length; i++) {
+//     reversedArr[i] *= 2;
+//   }
+
+//   return reversedArr;
+// }
+
