b12637e0cc8997ac80ee71418539f9465b457e8b
woojoung <ywj981217@naver.com>
2024-01-22 10:22:18 +0900

    day06 study final

--- 05.DAY01/client/main.js
+++ 05.DAY01/client/main.js
@@ -1,2 +1,68 @@
-console.log('hello world')
 
+/* eslint-disable */
+
+
+// function showMessage(from, text = anotherFunction()) {
+//   console.log(from + "  " + text);
+// }
+
+
+// function anotherFunction() {
+//   return "l like you "
+// }
+
+// showMessage("ANn", 'you are good')
+
+// function checkAge(age) {
+//   if (age >= 18) {
+//     return true;
+//   } else {
+//     return confirm('보호자의 동의를 받으셨나요?');
+//   }
+// }
+
+// let age = prompt('나이를 알려주세요', 18);
+
+// if (checkAge(age)) {
+//   alert('접속 허용');
+// } else {
+//   alert('접속 차단');
+// // }
+
+// function getAllPrimeNumToN(number) {
+//   for (let i = 2; i < number; i++) {
+//     let isPrime = true;
+//     for (let j = 2; j < i; j++) {
+//       if (i % j === 0) {
+//         isPrime = false;
+//         break;
+//       }
+//     }
+//     if (isPrime) {
+//       console.log(i); // 소수
+//     }
+//     console.log(isPrime)
+//   }
+// }
+
+// // console.log(isPrime)
+
+// // 예시로 getAllPrimeNumToN 함수 호출
+
+
+// getAllPrimeNumToN(20);
+
+
+
+function getPrimeNum(number) {
+  nextPrimeNum: for (let i = 2; i < number; i++) {
+    for (let j = 2; j < i; j++) {
+      continue
+      if (i % j === 0) {
+      }
+      console.log(i)
+    }
+  }
+}
+
+getPrimeNum(20)
---
+++ 05.DAY01/server/index.js
@@ -0,0 +1,11 @@
+const server = require('live-server');
+
+const params = {
+  host: 'localhost',
+  port: 3000,
+  open: false,
+  root: './client'
+}
+
+
+server.start(params);
